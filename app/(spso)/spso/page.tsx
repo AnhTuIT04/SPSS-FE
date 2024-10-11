fd866a4cff905f72985efb8211785973fadd5392
TuSunfua <tu.huynhit2901@hcmut.edu.vn>
2024-10-11 17:27:47 +0700

    first commit

---
+++ app/(spso)/spso/page.tsx
@@ -0,0 +1,14 @@
+import SideBar from "@/components/SideBar"
+
+export default function Home() {
+    return (
+        <>
+            <div className="flex ">
+                <SideBar />
+                <div>
+                    <h1>SPSO Home</h1>
+                </div>
+            </div>
+        </>
+    )
+}
