3d19e145f5da39f42e1b0946f149faa4f80bcc27
Han <han.lehoangngoc@hcmut.edu.vn>
2024-10-20 01:30:14 +0700

    add Select Printer

--- constants/student.ts
+++ constants/student.ts
@@ -27,3 +27,13 @@ export const studentSideBarLinks = [
     label: 'Settings',
   },
 ];
+
+export const printers = [
+  { id: 1, name: 'Máy in 1', location: 'Tầng 1', fileType: ['PDF', 'DOCX', 'JPEG'], status: 'Sẵn sàng' },
+  { id: 2, name: 'Máy in 2', location: 'Tầng 2', fileType: ['PDF'], status: 'Sẵn sàng' },
+  { id: 3, name: 'Máy in 3', location: 'Tầng 3', fileType: ['PDF', 'DOCX'], status: 'Đang bận' },
+  { id: 4, name: 'Máy in 4', location: 'Tầng 4', fileType: ['PDF', 'DOCX', 'JPEG'], status: 'Sẵn sàng' },
+  { id: 6, name: 'Máy in 5', location: 'Tầng 5', fileType: ['PDF', 'DOCX'], status: 'Sẵn sàng' },
+  { id: 7, name: 'Máy in 1', location: 'Tầng 1', fileType: ['PDF', 'DOCX', 'JPEG'], status: 'Sẵn sàng' },
+  { id: 8, name: 'Máy in 2', location: 'Tầng 2', fileType: ['PDF'], status: 'Sẵn sàng' },
+];
