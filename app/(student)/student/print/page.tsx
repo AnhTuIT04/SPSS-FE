c53387002a3aafac3257fd93d13337af002e807b
Han <han.lehoangngoc@hcmut.edu.vn>
2024-10-25 02:28:29 +0700

    add Print Props

--- app/(student)/student/print/page.tsx
+++ app/(student)/student/print/page.tsx
@@ -5,70 +5,101 @@ import styles from '@/app/(student)/student.module.css';
 import Image from 'next/image';
 import uploadFile from '@/public/assets/uploadfile.svg';
 import deleteFileBtn from '@/public/assets/deletefilebtn.svg';
+import imgPlaceholder from '@/public/assets/imgplaceholder.svg';
+import { Select, InputNumber, Radio, Input } from 'antd';
+import type { InputNumberProps, RadioChangeEvent } from 'antd';
 
 const Print = () => {
-  const [step, setStep] = useState('select-printer');
+    const [step, setStep] = useState('select-printer');
 
-  const handleNextStep = (nextStep: string) => {
-    setStep(nextStep);
-  };
+    const handleNextStep = (nextStep: string) => {
+        setStep(nextStep);
+    };
 
-  const handlePrevStep = (prevStep: string) => {
-    setStep(prevStep);
-  };
+    const handlePrevStep = (prevStep: string) => {
+        setStep(prevStep);
+    };
 
-  const [selectedPrinter, setSelectedPrinter] = useState(null);
+    const [selectedPrinter, setSelectedPrinter] = useState(null);
 
-  const selectPrinter = (printer) => {
-    setSelectedPrinter(printer);
-    alert(`Bạn đã chọn ${printer.name}. Tiếp tục sang bước upload tài liệu.`);
-  };
+    const selectPrinter = (printer) => {
+        setSelectedPrinter(printer);
+        console.log(`choose ${printer.name}`)
+        alert(`Bạn đã chọn ${printer.name}. Tiếp tục sang bước upload tài liệu.`);
+    };
 
-  return (
-    <div className="w-full h-full">
-      {step === 'select-printer' && (
-        <div>
-          <h1 className="text-2xl font-bold mb-4">Select Printer</h1>
-          <div className="flex items-center justify-center m-auto">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-              {printers.map((printer) => (
-                <div
-                  key={printer.id}
-                  className="bg-white shadow-md rounded-lg cursor-pointer hover:bg-blue-100 transition w-[300px] h-fit"
-                  // className={`p-6  h-auto rounded-[8px] ${printer.status === 'Offline' ? 'bg-gray-200' : 'bg-white border-[1px] border-[solid] border-[#d5d5d5]  hover:border-[#1488DB]'}`}
-                  // className="bg-white p-6 border-[1px] border-[solid] border-[#d5d5d5] rounded-[8px] h-auto hover:border-[#1488DB] "
-                  onClick={() => {
-                    selectPrinter(printer);
-                    handleNextStep('upload');
-                  }}
-                >
-                  <div className="h-[222px] bg-[#DEDEDE] rounded-tl-lg rounded-tr-lg"></div>
-                  <div className="p-4">
-                    <p className="text-xl font-bold my-[8px]">{printer.name}</p>
-                    <p>Status: {printer.status}</p>
-                    <hr className="my-4 border-gray-300" />
-                    <div>
-                      File Type:
-                      <div className="inline-flex">
-                        {printer.fileType &&
-                          printer.fileType.map((type, index) => (
-                            <div
-                              key={index}
-                              className="flex-row justify-center items-end mx-[2px] px-[12px] py-[8px] gap-[10px] w-fit h-[31px] border-[1px] border-[solid] border-[#546FFF] rounded-[100px] text-[12px] text-[#546FFF]"
-                            >
-                              {type}
-                            </div>
-                          ))}
-                      </div>
+    // Select
+    const handleChangeSelect = (value: string) => {
+        console.log(`selected ${value}`);
+    };
+
+    // Input
+    const onChangeInput: InputNumberProps['onChange'] = (value) => {
+        console.log('changed', value);
+    };
+
+    // Radio
+    const [value, setValue] = useState(1);
+    const onChangeRadio = (e: RadioChangeEvent) => {
+        console.log('radio checked', e.target.value);
+        setValue(e.target.value);
+        if (e.target.value === 1) {
+            setInputValue('');  // Clear input when selecting "All"
+        }
+    };
+    const [inputValue, setInputValue] = useState('');  // Store input value
+
+    // Handle input change and auto-select the second radio button
+    const onInputChange = (e) => {
+        setInputValue(e.target.value);
+        setValue(2);  // Auto-select value 2 when typing in the input
+    };
+
+    return (
+        <div className="w-full h-full">
+            {step === 'select-printer' && (
+                <div>
+                    <h1 className="text-2xl font-bold mb-4">Select Printer</h1>
+                    <div className="flex items-center justify-center m-auto">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                            {printers.map((printer) => (
+                                <div
+                                    key={printer.id}
+                                    className="bg-white shadow-md rounded-lg cursor-pointer hover:bg-blue-100 transition w-[300px] h-fit"
+                                    // className={`p-6  h-auto rounded-[8px] ${printer.status === 'Offline' ? 'bg-gray-200' : 'bg-white border-[1px] border-[solid] border-[#d5d5d5]  hover:border-[#1488DB]'}`}
+                                    // className="bg-white p-6 border-[1px] border-[solid] border-[#d5d5d5] rounded-[8px] h-auto hover:border-[#1488DB] "
+                                    onClick={() => {
+                                        selectPrinter(printer);
+                                        handleNextStep('upload');
+                                    }}
+                                >
+                                    <div className="h-[222px] bg-[#DEDEDE] rounded-tl-lg rounded-tr-lg"></div>
+                                    <div className="p-4">
+                                        <p className="text-xl font-bold my-[8px]">{printer.name}</p>
+                                        <p>Status: {printer.status}</p>
+                                        <hr className="my-4 border-gray-300" />
+                                        <div>
+                                            File Type:
+                                            <div className="inline-flex">
+                                                {printer.fileType &&
+                                                    printer.fileType.map((type, index) => (
+                                                        <div
+                                                            key={index}
+                                                            className="flex-row justify-center items-end mx-[2px] px-[12px] py-[8px] gap-[10px] w-fit h-[31px] border-[1px] border-[solid] border-[#546FFF] rounded-[100px] text-[12px] text-[#546FFF]"
+                                                        >
+                                                            {type}
+                                                        </div>
+                                                    ))}
+                                            </div>
+                                        </div>
+                                        <p className="text-gray-500 my-[8px]">Location: {printer.location}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    <p className="text-gray-500 my-[8px]">Location: {printer.location}</p>
-                  </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
+            )}
 
             {step === 'upload' && (
                 <div className=" justify-center max-w-screen-md mx-auto bg-white p-6 rounded-lg shadow-lg">
@@ -121,32 +152,95 @@ const Print = () => {
                 </div>
             )}
 
-      {step === 'specify-props' && (
-        <div>
-          <h2>Specify Printing Properties</h2>
-          <button className={styles.buttonBack} onClick={() => handlePrevStep('upload')}>
-            Back
-          </button>
-          <button className={styles.buttonNext} onClick={() => handleNextStep('review')}>
-            Next
-          </button>
-        </div>
-      )}
+            {step === 'specify-props' && (
+                <div className="flex justify-start max-w-screen-md mx-auto bg-white p-6 rounded-lg shadow-lg">
+                    <div className=' w-[50%]'>
+                        <Image src={imgPlaceholder} alt='img placeholder'></Image>
+                    </div>
+                    <div className='relative ml-[40px] w-[50%]'>
+                        <h1 className="mb-[4px] text-2xl font-bold">Print Properties</h1>
+                        <div className='flex mb-[16px]'>
+                            <div className='mr-[16px] w-[50%]'>
+                                <span>Page Size</span>
+                                <Select
+                                    className='block'
+                                    defaultValue="A4"
+                                    style={{ width: '100%' }}
+                                    onChange={handleChangeSelect}
+                                    options={[
+                                        { value: 'A4', label: 'A4' },
+                                        { value: 'A3', label: 'A3' },
+                                    ]}
+                                />
+                            </div>
+                            <div className=' w-[50%]'>
+                                <span>Copies</span>
+                                <InputNumber
+                                    className='block'
+                                    style={{ width: '100%' }}
+                                    min={1}
+                                    defaultValue={1}
+                                    onChange={onChangeInput}
+                                    changeOnWheel />
+                            </div>
+                        </div>
+                        <div className='mb-[16px]'>
+                            <span>Pages</span>
+                            <div>
+                                <Radio.Group className='ml-[24px]' onChange={onChangeRadio} value={value}>
+                                    <Radio className='block' value={1}>All</Radio>
+                                    <Radio className='flex' value={2}>
+                                        <Input
+                                            style={{ width: '100%' }}
+                                            placeholder="eg. 1-5, 8, 11-13"
+                                            disabled={value === 1}  // Disable input when "All" is selected
+                                            value={inputValue}
+                                            onChange={onInputChange}
+                                        />
+                                    </Radio>
 
-      {step === 'review' && (
-        <div>
-          <h2>Review and Submit</h2>
-          <button className={styles.buttonBack} onClick={() => handlePrevStep('specify-props')}>
-            Back
-          </button>
-          <button className={styles.buttonNext} onClick={() => handleNextStep('select-printer')}>
-            Start Over
-          </button>
-          <button>Submit</button>
+                                </Radio.Group>
+                            </div>
+                        </div>
+                        <div className='w-[100%] mb-[16px]'>
+                            <span>One-/double-sided</span>
+                            <Select
+                                className='block'
+                                defaultValue="One-sided"
+                                style={{ width: '100%' }}
+                                onChange={handleChangeSelect}
+                                options={[
+                                    { value: 'onesided', label: 'One-sided' },
+                                    { value: 'doublesided', label: 'Double-sided' },
+                                ]}
+                            />
+                        </div>
+                        <div className='absolute right-0 bottom-0 flex space-x-2 justify-end'>
+                            <button className={styles.buttonBack} onClick={() => handlePrevStep('upload')}>
+                                Back
+                            </button>
+                            <button className={styles.buttonNext} onClick={() => handleNextStep('review')}>
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {step === 'review' && (
+                <div>
+                    <h2>Review and Submit</h2>
+                    <button className={styles.buttonBack} onClick={() => handlePrevStep('specify-props')}>
+                        Back
+                    </button>
+                    <button className={styles.buttonNext} onClick={() => handleNextStep('select-printer')}>
+                        Start Over
+                    </button>
+                    <button>Submit</button>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Print;
