99d5f6a1a330f75f1f2fb5f26c195535d1fb9ac2
Han <han.lehoangngoc@hcmut.edu.vn>
2024-11-26 00:49:44 +0700

    Update Dashboard

--- app/(student)/student/dashboard/page.tsx
+++ app/(student)/student/dashboard/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Table, DatePicker, Spin, message } from 'antd'; import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+
+dayjs.extend(isBetween);
+
 import DashboardCard from '@/components/dashboard/DashboardCard';
 // import { dashboardCards } from "@/constants/student"
 import Image from 'next/image';
@@ -7,51 +15,127 @@ import trendUp from '@/public/assets/trend_up.svg';
 import trendDown from '@/public/assets/trend_down.svg';
 
 export default function Home() {
+    const { RangePicker } = DatePicker;
+
+    // Mock data
+    // const data = [
+    //     { id: 1, filename: 'doc1.pdf', size: '2MB', date: '2024-10-01', printer: 'Printer A', status: 'Completed' },
+    //     { id: 2, filename: 'doc2.docx', size: '3MB', date: '2024-10-03', printer: 'Printer B', status: 'Pending' },
+    //     { id: 3, filename: 'doc3.jpg', size: '1MB', date: '2024-10-05', printer: 'Printer C', status: 'Failed' },
+    //     { id: 4, filename: 'doc4.png', size: '500KB', date: '2024-10-07', printer: 'Printer A', status: 'Completed' },
+    //     { id: 5, filename: 'doc5.txt', size: '800KB', date: '2024-10-09', printer: 'Printer B', status: 'Pending' },
+    // ];
+
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // Fetch data from API
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('https://673760e4aafa2ef222339c88.mockapi.io/log'); // Replace with your API URL
+            if (!response.ok) throw new Error('Failed to fetch data');
+            const result = await response.json();
+            setData(result);
+            setFilteredData(result); // Initialize filtered data
+        } catch (error) {
+            message.error('Failed to load data. Please try again.');
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // Handle date range filtering
+    const handleDateChange = (dates) => {
+        if (!dates || dates.length === 0) {
+            setFilteredData(data); // Reset to original data if no date selected
+            return;
+        }
+
+        const [start, end] = dates;
+        const filtered = data.filter((item) => {
+            const itemDate = dayjs(item.date);
+            return itemDate.isBetween(start, end, null, '[]'); // Include start and end dates
+        });
+
+        setFilteredData(filtered);
+    };
+
+    // Table columns definition
+    const columns = [
+        { title: 'ID', dataIndex: 'id', key: 'id' },
+        { title: 'File Name', dataIndex: 'filename', key: 'filename' },
+        { title: 'Size', dataIndex: 'size', key: 'size' },
+        { title: 'Date', dataIndex: 'date', key: 'date' },
+        { title: 'Printer', dataIndex: 'printer', key: 'printer' },
+        { title: 'Status', dataIndex: 'status', key: 'status' },
+    ];
     return (
-        <div className="flex flex-col gap-2">
-            <div>
-                <p className="text-2xl font-bold">Dashboard</p>
-            </div>
-            {/* <div className="flex flex-wrap justify-evenly gap-4 p-4">
+        <div>
+            <div className="flex flex-col gap-2">
+                <div>
+                    <p className="text-2xl font-bold">Dashboard</p>
+                </div>
+                {/* <div className="flex flex-wrap justify-evenly gap-4 p-4">
                 {dashboardCards.map((card) => (
                     <DashboardCard key={card.label} {...card} trend={card.trend as "up" | "down"} />
                 ))}
             </div> */}
-            <div className='flex my-[16px]'>
-                <div className='relative p-[16px] mr-[32px] bg-white w-[250px] h-[140px] [box-shadow:6px_6px_54px_rgba(0,_0,_0,_0.05)] rounded-[14px]'>
-                    <div className='flex'>
-                        <div>
-                            <h2 className='text-[16px] text-[#636466]'>Page Balance</h2>
-                            <h3 className='text-[28px] font-semibold'>365</h3>
+                <div className='flex my-[16px]'>
+                    <div className='relative p-[16px] mr-[32px] bg-white w-[250px] h-[140px] [box-shadow:6px_6px_54px_rgba(0,_0,_0,_0.05)] rounded-[14px]'>
+                        <div className='flex'>
+                            <div>
+                                <h2 className='text-[16px] text-[#636466]'>Page Balance</h2>
+                                <h3 className='text-[28px] font-semibold'>365</h3>
+                            </div>
+                            <Image className='absolute right-[16px]'
+                                src={pageBalance} alt='pageBalance' width={50}></Image>
                         </div>
-                        <Image className='absolute right-[16px]'
-                            src={pageBalance} alt='pageBalance' width={50}></Image>
-                    </div>
-                    <div className='flex absolute bottom-[16px]'>
-                        <Image className='mr-[4px]' src={trendUp} alt='trendUp'></Image>
-                        <span className='text-[14px] text-[#636466]'>
-                            <span className='text-[#00B69B]'>+15 </span>
-                            pages from SPSO</span>
-                    </div>
-                </div>
-                <div className='relative p-[16px] mr-[32px] bg-white w-[250px] h-[140px] [box-shadow:6px_6px_54px_rgba(0,_0,_0,_0.05)] rounded-[14px]'>
-                    <div className='flex'>
-                        <div>
-                            <h2 className='text-[16px] text-[#636466]'>Total Pages Printed</h2>
-                            <h3 className='text-[28px] font-semibold'>293</h3>
+                        <div className='flex absolute bottom-[16px]'>
+                            <Image className='mr-[4px]' src={trendUp} alt='trendUp'></Image>
+                            <span className='text-[14px] text-[#636466]'>
+                                <span className='text-[#00B69B]'>+15 </span>
+                                pages from SPSO</span>
                         </div>
-                        <Image className='absolute right-[16px]'
-                            src={pagePrinted} alt='pageBalance' width={50}></Image>
                     </div>
-                    <div className='flex absolute bottom-[16px]'>
-                        <Image className='mr-[4px]' src={trendDown} alt='trendDown'></Image>
-                        <span className='text-[14px] text-[#636466]'>
-                            <span className='text-[#F93C65]'>4,3% </span>
-                            down from yesterday</span>
+                    <div className='relative p-[16px] mr-[32px] bg-white w-[250px] h-[140px] [box-shadow:6px_6px_54px_rgba(0,_0,_0,_0.05)] rounded-[14px]'>
+                        <div className='flex'>
+                            <div>
+                                <h2 className='text-[16px] text-[#636466]'>Total Pages Printed</h2>
+                                <h3 className='text-[28px] font-semibold'>293</h3>
+                            </div>
+                            <Image className='absolute right-[16px]'
+                                src={pagePrinted} alt='pageBalance' width={50}></Image>
+                        </div>
+                        <div className='flex absolute bottom-[16px]'>
+                            <Image className='mr-[4px]' src={trendDown} alt='trendDown'></Image>
+                            <span className='text-[14px] text-[#636466]'>
+                                <span className='text-[#F93C65]'>4,3% </span>
+                                down from yesterday</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+
+            <div style={{ padding: 20 }}>
+                {/* Date Range Picker */}
+                <div style={{ marginBottom: 20 }}>
+                    <RangePicker onChange={handleDateChange} />
+                </div>
+                {/* Table */}
+                {loading ? (
+                    <Spin tip="Loading..." />
+                ) : (
+                    <Table columns={columns} dataSource={filteredData} rowKey="id" />
+                )}
+            </div>
+        </div>
     );
 }
--- app/(student)/student/profile/page.tsx
+++ app/(student)/student/profile/page.tsx
@@ -1,31 +1,59 @@
 'use client'
 import React from 'react'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import studentAvatar from '@/public/assets/student.svg';
 import Image from 'next/image';
 
 const Page = () => {
-    const [profile, setProfile] = useState({
-        name: 'Nguyen Van A',
-        email: 'nguyenvana@example.com',
-        ID: '2210001',
-        gender: 'Female',
-        class: 'MT22KH08',
-        faculty: 'Computer Science & Engineering',
-        address: '123 Street, Ho Chi Minh City',
-    });
+    // const [student, setStudent] = useState({
+    //     name: 'Nguyen Van A',
+    //     email: 'nguyenvana@example.com',
+    //     ID: '2210001',
+    //     gender: 'Female',
+    //     class: 'MT22KH08',
+    //     faculty: 'Computer Science & Engineering',
+    //     address: '123 Street, Ho Chi Minh City',
+    // });
+
+    const [student, setStudent] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // Gửi request đến API
+    useEffect(() => {
+        const fetchStudent = async () => {
+            try {
+                const response = await fetch('https://673760e4aafa2ef222339c88.mockapi.io/student/2'); // Thay bằng URL của API
+                if (!response.ok) {
+                    throw new Error('Failed to fetch student data');
+                }
+                const data = await response.json();
+                setStudent(data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchStudent();
+    }, []);
+
+    // Hiển thị loading hoặc lỗi
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className='h-[540px] w-[1200px] bg-[#ffffff] rounded-[9.36111px] p-8'>
-            <div className="h-fit flex flex-row">
+        <div className='h-[540px] w-[1100px] bg-[#ffffff] rounded-[9.36111px] p-8'>
+            <div className="flex flex-row">
                 <Image
                     src={studentAvatar}
                     alt="Profile"
                     className="w-20 h-20 rounded-full border-2 border-gray-300 object-cover mr-4"
                 ></Image>
                 <div className='flex flex-col justify-center'>
-                    <span className='font-medium text-[20px]'>{profile.name}</span>
-                    <span className='font-medium text-[15px] text-[#000000] opacity-50'>{profile.email}</span>
+                    <span className='font-medium text-[20px]'>{student.name}</span>
+                    <span className='font-medium text-[15px] text-[#000000] opacity-50'>{student.email}</span>
                 </div>
             </div>
             <div className='flex gap-24'>
@@ -34,24 +62,24 @@ const Page = () => {
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Full Name</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.name}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.name}
                         </div>
                     </div>
                     {/* Gender */}
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Gender</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.gender}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.gender}
                         </div>
                     </div>
                     {/* Faculty */}
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Faculty</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.faculty}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.faculty}
                         </div>
                     </div>
                 </div>
@@ -60,24 +88,24 @@ const Page = () => {
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Student ID</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.ID}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.ID}
                         </div>
                     </div>
                     {/* Address */}
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Address</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.address}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.address}
                         </div>
                     </div>
                     {/* Class */}
                     <div className='my-[24px]'>
                         <div className='text-[15px] text-[#000000]'>Class</div>
                         <div
-                            className='flex items-center my-[8px] px-[24px] w-[500px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
-                            {profile.class}
+                            className='flex items-center my-[8px] px-[24px] w-[450px] h-[40px] bg-[#f9f9f9] rounded-[7.5px] text-[15px] text-[#959595]'>
+                            {student.class}
                         </div>
                     </div>
                 </div>
