a11b39b9f63809dc419bfbd4d28c1604e1b3cc27
VinhLe2213963 <vinh.lekhanhvinh@hcmut.edu.vn>
2024-12-05 02:16:21 +0700

    Add GET and POST for Report, PaymentLog, PrintingLog and Printer

---
+++ app/api/v1/report/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+
+import { connectDB } from "@/db/connect";
+import { Report } from "@/models";
+
+
+export async function POST(req: any) {
+
+    const AppDataSource = await connectDB();
+    const reportRepository = AppDataSource.getRepository(Report);
+
+    try {
+        const body = await req.json();
+
+        const report = reportRepository.create({
+            name: body.name,
+            date: body.date,
+            type: body.type
+        });
+        await reportRepository.save(report);
+
+
+        return NextResponse.json({ ...report });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+    }
+}
+
+export async function GET () {
+    const AppDataSource = await connectDB();
+    const reportRepository = AppDataSource.getRepository(Report);
+    try {
+        const printingLogs = await reportRepository.find();
+
+        return NextResponse.json({ printingLogs });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+    }
+}
